71228dc67f08c6aad5bc1f88599517d0813963b1
TiAmoLip <1239261229@qq.com>
2024-04-19 20:10:36 +0800

    update lr

--- LM/scripts/larger_run.sh
+++ LM/scripts/larger_run.sh
@@ -1,14 +1,14 @@
 mode=$1
 if [ $mode == "first" ]; then
-    python main.py --data gigaspeech --cuda --epochs 40 --model L --optimizer --rope --hiddens 2048 --nlayers 16 --lr 0.01 --wandb --run_name L_rope_adam_2048hid_16layers
+    python main.py --data gigaspeech --cuda --epochs 40 --model L --optimizer --rope --hiddens 2048 --nlayers 16 --lr 0.003 --wandb --run_name L_rope_adam_2048hid_16layers
 fi
 
 if [ $mode == "second" ]; then
-    python main.py --data gigaspeech --cuda --epochs 40 --model L --optimizer --rope --hiddens 2048 --nlayers 16 --lr 0.01 --nhead 16 --wandb --run_name L_rope_adam_2048hid_16layers_16nhead
+    python main.py --data gigaspeech --cuda --epochs 40 --model L --optimizer --rope --hiddens 2048 --nlayers 16 --lr 0.003 --nhead 16 --wandb --run_name L_rope_adam_2048hid_16layers_16nhead
 fi
 
 if [ $mode == "third" ]; then
-    python main.py --data gigaspeech --cuda --epochs 40 --model L --optimizer --rope --hiddens 2048 --nlayers 12 --lr 0.01 --nhead 16 --embed_size 384 --wandb --run_name L_rope_adam_2048hid_12layers_16nhead_512embed
+    python main.py --data gigaspeech --cuda --epochs 40 --model L --optimizer --rope --hiddens 2048 --nlayers 12 --lr 0.003 --nhead 16 --embed_size 384 --wandb --run_name L_rope_adam_2048hid_12layers_16nhead_512embed
 fi
 
 # larger parameters:
--- LM/scripts/run.sh
+++ LM/scripts/run.sh
@@ -2,18 +2,18 @@ mode=$1
 
 # with rope and adam
 if [ $mode == "rope_adam" ]; then
-    python main.py --data gigaspeech --cuda --epochs 40 --model L --optimizer --rope --wandb --run_name L_rope_adam
+    python main.py --data gigaspeech --cuda --epochs 40 --model L --optimizer --rope --wandb --run_name L_rope_adam --lr 0.003
 fi
 
 if [ $mode == "adam" ]; then
-    python main.py --data gigaspeech --cuda --epochs 40 --model L --optimizer --wandb --run_name L_adam
+    python main.py --data gigaspeech --cuda --epochs 40 --model L --optimizer --wandb --run_name L_adam --lr 0.003
 fi
 
 if [ $mode == "sgd" ]; then
-    python main.py --data gigaspeech --cuda --epochs 40 --model L --wandb --run_name L_sgd
+    python main.py --data gigaspeech --cuda --epochs 40 --model L --wandb --run_name L_sgd --lr 0.003
 fi
 
 if [ $mode == "rope_sgd" ]; then
-    python main.py --data gigaspeech --cuda --epochs 40 --model L --rope --wandb --run_name L_rope_sgd
+    python main.py --data gigaspeech --cuda --epochs 40 --model L --rope --wandb --run_name L_rope_sgd --lr 0.003
 fi
 
