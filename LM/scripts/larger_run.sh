d11825e16c6d9a076cc6428cd39bbe3bcbb7e2d5
TiAmoLip <1239261229@qq.com>
2024-04-20 13:02:22 +0800

    modify scripts

--- LM/scripts/larger_run.sh
+++ LM/scripts/larger_run.sh
@@ -8,7 +8,7 @@ if [ $mode == "second" ]; then
 fi
 
 if [ $mode == "third" ]; then
-    python main.py --data gigaspeech --cuda --epochs 40 --model L --optimizer --rope --hiddens 2048 --nlayers 12 --lr 0.003 --nhead 16 --embed_size 384 --wandb --run_name L_rope_adam_2048hid_12layers_16nhead_512embed
+    python main.py --data gigaspeech --cuda --epochs 40 --model L --optimizer --rope --hiddens 2048 --nlayers 12 --lr 0.003 --nhead 16 --embed_size 384 -bptt 64 --wandb --run_name L_rope_adam_2048hid_12layers_16nhead_512embed
 fi
 
 # larger parameters:
