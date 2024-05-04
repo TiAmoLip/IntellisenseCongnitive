a3beea26744252d077de20f3bb342fd76faacc9b
TiAmoLip <1239261229@qq.com>
2024-05-04 15:37:08 +0800

    update sed

---
+++ sound_event_detection/models.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+
+
+def linear_softmax_pooling(x):
+    return (x ** 2).sum(1) / x.sum(1)
+
+
+class Crnn(nn.Module):
+    def __init__(self, num_freq, num_class):
+        ##############################
+        # YOUR IMPLEMENTATION
+        # Args:
+        #     num_freq: int, mel frequency bins
+        #     num_class: int, the number of output classes
+        ##############################
+        pass
+
+    def detection(self, x):
+        ##############################
+        # YOUR IMPLEMENTATION
+        # Args:
+        #     x: [batch_size, time_steps, num_freq]
+        # Return:
+        #     frame_prob: [batch_size, time_steps, num_class]
+        ##############################
+        pass
+
+    def forward(self, x):
+        frame_prob = self.detection(x)  # (batch_size, time_steps, class_num)
+        clip_prob = linear_softmax_pooling(frame_prob)  # (batch_size, class_num)
+        '''(samples_num, feature_maps)'''
+        return {
+            'clip_prob': clip_prob, 
+            'frame_prob': frame_prob
+        }
